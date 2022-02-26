894c0f23e6dd97735438652a365e64961aed1d42
sallytheseal13 <23ajitm@orcsd.org>
2022-02-26 11:43:59 -0500

    Created HangerSubsystem
    created updated hanger subsystem with encoders

---
+++ src/main/java/frc/robot/team8410/subsystems/HangerSubsystem.java
@@ -0,0 +1,71 @@
+package frc.robot.team8410.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
+public class HangerSubsystem extends SubsystemBase {
+    
+    PWMVictorSPX armMotor1 = new PWMVictorSPX(6);
+    PWMVictorSPX armMotor2 = new PWMVictorSPX(7);
+    MotorControllerGroup armGroup = new MotorControllerGroup(armMotor1, armMotor2);
+    PWMVictorSPX clipMotor1 = new PWMVictorSPX(8);
+    PWMVictorSPX clipMotor2 = new PWMVictorSPX(9);
+    MotorControllerGroup clipGroup = new MotorControllerGroup(clipMotor1, clipMotor2);
+    
+    private DutyCycleEncoder arm_Encoder = new DutyCycleEncoder(0);
+    private DutyCycleEncoder clip_Encoder = new DutyCycleEncoder(1);
+
+    
+    public void armUp(double upDist){
+        arm_Encoder.reset();
+        if(arm_Encoder.getDistance() < upDist){
+            armGroup.set(1.0);
+        }
+        else{
+            armGroup.set(0.0);
+        }
+    }
+
+    public void armDown(double downDist){
+        arm_Encoder.reset();
+        armGroup.setInverted(true);
+        if(arm_Encoder.getDistance() < downDist){
+            armGroup.set(1.0);
+        }
+        else{
+            armGroup.set(0.0);
+        }
+    }
+    
+    public void clipUp(double clipUpDist){
+        clip_Encoder.reset();
+        if(clip_Encoder.getDistance() < clipUpDist){
+            clipGroup.set(1.0);
+        }
+        else{
+            clipGroup.set(0.0);
+        }
+    }
+
+    public void clipDown(double clipDownDist){
+        clip_Encoder.reset();
+        if(clip_Encoder.getDistance() < clipDownDist){
+            clipGroup.set(1.0);
+        }
+        else{
+            clipGroup.set(0.0);
+        }
+    }
+
+
+    public HangerSubsystem() {}
+
+    @Override
+    public void periodic() {
+      arm_Encoder.setDistancePerRotation(1.0);
+      clip_Encoder.setDistancePerRotation(1.0);
+    }
+    // This method will be called once per scheduler run
+}
