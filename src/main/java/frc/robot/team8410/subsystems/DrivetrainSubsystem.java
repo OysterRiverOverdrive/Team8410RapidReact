2f2b32dbd1c852418aa5c1c727b98ac7c949d5b3
Banana123abcd <anikapant@gmail.com>
2022-02-28 15:35:42 -0500

    made the drive command

--- src/main/java/frc/robot/team8410/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/team8410/subsystems/DrivetrainSubsystem.java
@@ -8,29 +8,44 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.Joystick;
 
+
+
 public class DrivetrainSubsystem extends SubsystemBase {
 
   // in our robot we have two motors on left
-  private final TalonSRX m_left1 = new TalonSRX(0);
-  private final TalonSRX m_left2 = new TalonSRX(1);
-  //MotorControllerGroup liftSide = new MotorControllerGroup(m_left1, m_left2);
+  private final WPI_TalonSRX m_left1 = new WPI_TalonSRX(0);
+  private final WPI_TalonSRX m_left2 = new WPI_TalonSRX(1);
+  MotorControllerGroup leftSide = new MotorControllerGroup(m_left1, m_left2);
+
 
   // in our robot we have two motors on right
-  private final TalonSRX m_right1 = new TalonSRX(2);
-  private final TalonSRX m_right2 = new TalonSRX(3);
-  //MotorControllerGroup m_right = new MotorControllerGroup(m_right1, m_right2);
+  private final WPI_TalonSRX m_right1 = new WPI_TalonSRX(2);
+  private final WPI_TalonSRX m_right2 = new WPI_TalonSRX(3);
+  MotorControllerGroup rightSide = new MotorControllerGroup(m_right1, m_right2);
 
   // we use diffrential drive
-  //private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_left, m_right);
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(leftSide, rightSide);
 
   // we use two joysticks.
   private final Joystick m_stick = new Joystick(0);
   /** Creates a new DrivetrainSubsystem. */
-  public DrivetrainSubsystem() {}
+  public DrivetrainSubsystem() 
+  {
+
+  }
+
+  public void driveTheBot ()
+  {
+    m_robotDrive.arcadeDrive(m_stick.getRawAxis(4) * 0.75, m_stick.getRawAxis(1)*-0.85);
+  }
 
   @Override
   public void periodic() {
