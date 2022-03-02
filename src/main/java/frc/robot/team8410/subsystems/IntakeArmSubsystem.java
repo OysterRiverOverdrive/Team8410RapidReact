5dd8f9bfffc03a717deac81d4669d313a438d720
Dominic Couture <45298797+mastermind-mayhem@users.noreply.github.com>
2022-03-02 16:14:50 -0500

    Intake Subsystem Base

--- src/main/java/frc/robot/team8410/subsystems/IntakeArmSubsystem.java
+++ src/main/java/frc/robot/team8410/subsystems/IntakeArmSubsystem.java
@@ -4,28 +4,49 @@
 
 package frc.robot.team8410.subsystems;
 
-import edu.wpi.first.wpilibj.Joystick;
+// import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;  
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX; 
+
 
 
 public class IntakeArmSubsystem extends SubsystemBase {
 
-  private DutyCycleEncoder intakearm = new DutyCycleEncoder(1);
+  // private DutyCycleEncoder intakearm = new DutyCycleEncoder(1);
 
-  private final TalonSRX m_lift = new TalonSRX(4); 
+  private final WPI_TalonSRX shooterUp = new WPI_TalonSRX(5);
+  private final WPI_TalonSRX shooterArm = new WPI_TalonSRX(5);
 
-  private final Joystick m_stick = new Joystick(5);
+  // private final Joystick shooterUp = new Joystick(6);
 
-  /** Creates a new IntakeArmSubsystem. */
-  public IntakeArmSubsystem() {}
-
+  // private static final int kJoystickPort = 1;
+  // // Sets shooter joystick port
+  // private Joystick  m_joystick;
+  // double m_joystick = new Joystick(kJoystickPort);
+  // double joystickinput = m_joystick;
+  
   @Override
   public void periodic() {
-
-    intakearm.setDistancePerRotation(1);
-
-    // This method will be called once per scheduler run
+    
+  }
+  public void intakepull () {
+    shooterUp.set(0.75);
+    
+  }
+  public void intakepush () {
+    shooterUp.set(-0.75);
+    
+  }
+  public void intakeraise () {
+    shooterArm.set(0.5);
+    
+  }
+  public void intakedrop () {
+    shooterArm.set(-0.5);
+    
+  }
+  public void stopShoot () {
+    shooterUp.set(0);
+    // Stop shooter group
   }
 }
