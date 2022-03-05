870d2b5c741ddc56f7d4abdcdaac37be92f13e7a
Dominic Couture <45298797+mastermind-mayhem@users.noreply.github.com>
2022-03-05 11:33:25 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -69,11 +69,11 @@ public class Robot extends TimedRobot
 
     /*******/
     // This is where we will read the sensors and call the set method of the sensorValue object
-    //  This also needs the ultrasonic values
+    //  Commented out until problem is found
 
-    sensorValues.setUltrasonicBackInches(UltrasonicBack.getBackSensorDistance());
-    sensorValues.setUltrasonicLeftInches(UltrasonicLeft.getLeftSensorDistance());
-    sensorValues.setUltrasonicRightInches(UltrasonicRight.getRightSensorDistance());
+    //sensorValues.setUltrasonicBackInches(UltrasonicBack.getBackSensorDistance());
+    //sensorValues.setUltrasonicLeftInches(UltrasonicLeft.getLeftSensorDistance());
+    //sensorValues.setUltrasonicRightInches(UltrasonicRight.getRightSensorDistance());
     
      if(colorTCSSensor.isRed())
      {
