5aab1c507434c3768fcb092b02863afa34d2480c
sallytheseal13 <23ajitm@orcsd.org>
2022-03-09 21:04:48 -0500

    Renaming 'Sequential Test' to new 'Hang Cmd'

--- src/main/java/frc/robot/team8410/commands/hangCmd.java
+++ src/main/java/frc/robot/team8410/commands/hangCmd.java
@@ -12,9 +12,9 @@ import frc.robot.team8410.subsystems.WinchSubsystem;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class SequentialTest extends SequentialCommandGroup {
+public class hangCmd extends SequentialCommandGroup {
   /** Creates a new SequentialTest. */
-  public SequentialTest(WinchSubsystem winch, TwoStageClimber twoStage, OneStageClimber oneStage) {
+  public hangCmd(WinchSubsystem winch, TwoStageClimber twoStage, OneStageClimber oneStage) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands();
