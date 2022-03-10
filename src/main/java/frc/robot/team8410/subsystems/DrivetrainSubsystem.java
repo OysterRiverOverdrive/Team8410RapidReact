2a28d6971ca4e0ee1103a947d61f3e86f144cd31
Ashish <ashishpant22@yahoo.com>
2022-03-09 19:20:47 -0500

    real vs pracrice bot

--- src/main/java/frc/robot/team8410/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/team8410/subsystems/DrivetrainSubsystem.java
@@ -4,7 +4,9 @@
 
 package frc.robot.team8410.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+//import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX; // this is real bot
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX; // this is the practice bot
+
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -17,14 +19,14 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
   // in our robot we have two motors on left
 
-  private final WPI_TalonSRX m_left1 = new WPI_TalonSRX(0);
-  private final WPI_TalonSRX m_left2 = new WPI_TalonSRX(1);
+  private final WPI_VictorSPX m_left1 = new WPI_VictorSPX(0);
+  private final WPI_VictorSPX m_left2 = new WPI_VictorSPX(1);
   MotorControllerGroup leftSide = new MotorControllerGroup(m_left1, m_left2);
 
 
   // in our robot we have two motors on right
-  private final WPI_TalonSRX m_right1 = new WPI_TalonSRX(2);
-  private final WPI_TalonSRX m_right2 = new WPI_TalonSRX(3);
+  private final WPI_VictorSPX m_right1 = new WPI_VictorSPX(2);
+  private final WPI_VictorSPX m_right2 = new WPI_VictorSPX(3);
   MotorControllerGroup rightSide = new MotorControllerGroup(m_right1, m_right2);
 
   // we use diffrential drive
