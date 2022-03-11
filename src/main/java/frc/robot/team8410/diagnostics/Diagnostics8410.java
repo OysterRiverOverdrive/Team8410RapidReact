eea98c2b49dbdb46d35b2d8675ed4e0ba2187805
Dan Couture <2135547+mathyourlife@users.noreply.github.com>
2022-03-11 18:03:11 -0500

    Mathyourlife/court cherry pick (#37)
    * adding courtney's diagnostic
    
    * removing hanger commands that will come back later

--- src/main/java/frc/robot/team8410/diagnostics/Diagnostics8410.java
+++ src/main/java/frc/robot/team8410/diagnostics/Diagnostics8410.java
@@ -8,17 +8,19 @@ package frc.robot.team8410.diagnostics;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.team8410.commands.TeleopDriveCommand;
-
 import frc.robot.team8410.sensors.SensorValues;
 import frc.robot.team8410.subsystems.DrivetrainSubsystem;
-//import frc.robot.team8410.subsystems.HangerSubsystem;
 
 /** Add your docs here. */
 public class Diagnostics8410 
 {
+ 
+   private final DrivetrainSubsystem drivetrain = new DrivetrainSubsystem();
+   private final TeleopDriveCommand teleopCommand = new TeleopDriveCommand(drivetrain);
 
- 
- 
+   private final PowerDistribution powerDistribution = new PowerDistribution(0, PowerDistribution.ModuleType.kCTRE);
+   //powerDistribution
+
    public Diagnostics8410()
    {
 
@@ -42,10 +44,55 @@ public class Diagnostics8410
        SmartDashboard.putNumber("Ultrasonic Right", sensorValues.getUltrasonicRightInches());
 
 
+     // displaying subsystems
+       SmartDashboard.putData(drivetrain);
+
+      //display commands
+      SmartDashboard.putData(teleopCommand);
+      SmartDashboard.putData(teleopCommand);
+
     
+      //displaying power distribution
+      SmartDashboard.putNumber("Temperature", powerDistribution.getTemperature());
+      SmartDashboard.putNumber("Total Current", powerDistribution.getTotalCurrent());
+      SmartDashboard.putNumber("Voltage", powerDistribution.getVoltage());
+      SmartDashboard.putNumber("Power", powerDistribution.getTotalPower());
+    
+// make this to a constant
+      if (powerDistribution.getTemperature() > 35) {
+      SmartDashboard.putBoolean("Temp is High", false);
+    } else {
+      SmartDashboard.putBoolean("Temp is Ok", true);
+    }
+
+
+      if (powerDistribution.getTotalCurrent() < 20) {
+      SmartDashboard.putBoolean("Current is High", false);
+    } else {
+      SmartDashboard.putBoolean("Current is Fine", true);
+    }
+
+
+       if (powerDistribution.getVoltage() > 1 ) {
+      SmartDashboard.putBoolean("Voltage is High", true);
+    } else {
+      SmartDashboard.putBoolean("Voltage is Fine", false);
+    }
+
+    
+      if (powerDistribution.getTotalPower() < 1) {
+      SmartDashboard.putBoolean("Power is Fine", true);
+    } else {
+      SmartDashboard.putBoolean("Power is High", false);
+
+
+    
+    SmartDashboard.putString("Testing Shuffleboard", "Testing Now");
+    
+
     }
        
    }
 
 
-
+}
