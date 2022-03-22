38110580b70e07052304333009d9b1eb3a8d53a8
Dan Couture <MathYourLife@gmail.com>
2022-03-22 19:15:53 -0400

    txt back to java

--- src/main/java/frc/robot/team8410/commands/DriverAutoCmd.java
+++ src/main/java/frc/robot/team8410/commands/DriverAutoCmd.java
@@ -7,15 +7,16 @@ package frc.robot.team8410.commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.team8410.subsystems.IntakeArmSubSystem;
 import frc.robot.team8410.subsystems.DrivetrainSubsystem;
+import frc.robot.team8410.sensors.PotSensor;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class DriverAutoCmd extends SequentialCommandGroup {
   /** Creates a new DriverAutoCmd. */
-  public DriverAutoCmd(DrivetrainSubsystem drive, IntakeArmSubSystem intake) {
+  public DriverAutoCmd(DrivetrainSubsystem drive, IntakeArmSubSystem intake, PotSensor potSensor) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new RaiseIntakeCmd(intake), new AutoDriveTargetCmd(drive));
+    addCommands(new RaiseIntakeCmd(intake, potSensor), new AutoDriveTargetCmd(drive));
   }
 }
